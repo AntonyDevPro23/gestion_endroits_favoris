47e5bb380dc1dd8c3c626f1cfd564d6c1bdd450e
Development student at sayna and DCLIC <kidisodahlon@gmail.com>
2025-08-08 08:44:54 +0000

    screens

---
+++ liste_endroits.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+class Endroit {
+  final String id;
+  final String titre;
+  final File image;
+
+  Endroit({
+    required this.id,
+    required this.titre,
+    required this.image,
+  });
+}
+
+
