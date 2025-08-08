2ae0457ed4e4dafcb5454d62a43746a8abb2acf5
Development student at sayna and DCLIC <kidisodahlon@gmail.com>
2025-08-08 08:50:31 +0000

    fichier lib

---
+++ main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gestion_endroits_favoris/vue/endroits_interface.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Endroits favoris',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: const EndroitsInterface(),
+    );
+  }
+}
+
+
+
+
